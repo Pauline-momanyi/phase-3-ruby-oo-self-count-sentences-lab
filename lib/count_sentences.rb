50189fc6f29d825307e65a9262256e30834d7124
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-09 21:03:09 +0300

    commit tree bfa8caa159563f9e460a6aeb17939bd82542b9e4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
+    # self[self.length-1] == '.'? true:false
+    self.end_with?('.')
     
   end
 
   def question?
+    self.end_with?('?')
 
   end
 
   def exclamation?
+    self.end_with?('!')
 
   end
 
   def count_sentences
+    self.split(/[\.\!\?]+/).count
 
   end
 end
+
+word = 'hello here'
+len = word.length
+puts word[len-1]
+puts 'hellose.'.sentence?
+puts "This is a string! It has three sentences. Right?".count_sentences
+puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
